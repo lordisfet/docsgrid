c67b397049e995fd9ff109d09161a2dc911600a1
lordisfet <143896699+lordisfet@users.noreply.github.com>
2025-04-18 19:42:59 +0300

    Initial commit

---
+++ docsgrid/src/main/java/com/example/docsgrid/DocsgridApplication.java
@@ -0,0 +1,13 @@
+package com.example.docsgrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsgridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsgridApplication.class, args);
+	}
+
+}
---
+++ docsgrid/src/test/java/com/example/docsgrid/DocsgridApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docsgrid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocsgridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
